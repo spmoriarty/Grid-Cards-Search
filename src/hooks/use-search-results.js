a0a7c93e48ba53b620ec94c6ca7ad7937f81f0f2
Sean Moriarty <spmoriarty1@gmail.com>
2022-10-03 11:49:50 -0700

    hooks hydrated

--- src/hooks/use-search-results.js
+++ src/hooks/use-search-results.js
@@ -1,15 +1,54 @@
-import { useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import  { useSearchParams } from 'react-router-dom';
+import { search } from '../services/pokedex';
+import { useInView } from 'react-intersection-observer';
 
-export default function useSearchForm() {
+export default function useSearchResults() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [error, setError] = useState('');
   const [searchParams, setSearchParams] = useSearchParams();
+
   const useableSearchParams = Object.fromEntries(searchParams.entries());
-  const [pokemon, setPokemon] = useState(useableSearchParams.pokemon || '');
+
+  const nextPage = async () => {
+    useableSearchParams.page = parseInt(useableSearchParams.page) + 1;
+    setSearchParams(useableSearchParams);
+    const moreResults = await search(useableSearchParams);
+    setSearchResults(searchResults.concat(moreResults.results));
+  };
+    
+  const infiniteScrollRef = useInView({
+    triggerOnce:true,
+    onchange: (inView) => {
+      if (inView) nextPage();
+    },
+  }).ref;
+
+  const searchPokedex = async (searchObj) => {
+    if (searchObj.page == null) {
+      search.page = 1;
+    }
+    setSearchParams(searchObj);
+    try {
+      const body = await search(searchObj);
+      setSearchResults(body.results);
+    } catch(e) {
+      console.log(e);
+      setError('Error' + e.toString());
+    }
+  };
+
+  useEffect(() => void searchPokedex(useableSearchParams), []);
 
   return {
-    pokemon,
-    searchParams: useableSearchParams,
-    setPokemon,
-    setSearchParams
+    nextPage,
+    searchParams,
+    searchResults,
+    setSearchResults,
+    searchPokedex,
+    infiniteScrollRef,
+    error,
   };
+
 }
+
