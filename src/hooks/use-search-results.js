a3ca8ce060f0769b5e67fcff6853b851eec270ce
Sean Moriarty <spmoriarty1@gmail.com>
2022-10-13 10:21:06 -0700

    added process and secrets

--- src/hooks/use-search-results.js
+++ src/hooks/use-search-results.js
@@ -33,7 +33,7 @@ export default function useSearchResults() {
       const body = await search(searchObj);
       setSearchResults(body.results);
     } catch(e) {
-      console.log(e);
+      // console.log(e);
       setError('Error' + e.toString());
     }
   };
